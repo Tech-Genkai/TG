5dcaf6b6027e17c2f3a2e872296219396623a4c2
Aayush0p4r <159019813+Aayush0p4r@users.noreply.github.com>
2025-03-26 20:30:40 +0530

    v.1

--- src/script.js
+++
@@ -1,7 +0,0 @@
-<script>
-        async function fetchData() {
-            const response = await fetch("http:// 192.168.29.106:5000/");
-            const data = await response.text();
-            document.getElementById("output").innerText = data;
-        }
-    </script>
